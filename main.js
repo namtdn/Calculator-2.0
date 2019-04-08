1fd6ed19439fffd97ef0234d93b21ee51e9f252f
huong9x <huongnv@sphinx-software.com>
2019-04-08 09:26:16 +0700

    adding try catch

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
 const calculator = require('./calculator');
 cal = new  calculator();
-console.log(cal.do(process.argv[2], process.argv[3], process.argv[4]));
-console.log(process.argv);
+try {
+    console.log(cal.do(process.argv[2], process.argv[3], process.argv[4]));
+} catch (error) {
+    console.log(error);
+}
+// console.log(process.argv);
